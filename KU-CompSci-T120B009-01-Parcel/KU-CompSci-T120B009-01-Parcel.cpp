b00f29af3ed54bfeb1af6f73ee34d42131e25b1c
MT <mantas@tumenas.eu>
2021-09-19 19:21:24 +0300

    Fix main class filename in doc, this time for sure

--- KU-CompSci-T120B009-01-Parcel/KU-CompSci-T120B009-01-Parcel.cpp
+++ KU-CompSci-T120B009-01-Parcel/KU-CompSci-T120B009-01-Parcel.cpp
@@ -1,5 +1,5 @@
 ﻿/**
- * KU-CompSci-T120B009-02-Nine_Tasks.cpp
+ * KU-CompSci-T120B009-01-Parcel.cpp
  *
  * 1 uždavinys: "Siuntinys" (banderolė, registruotas laiškas, kasininkės uždavinys).
  * Apskaičiuoti, kiek mažiausiai pašto ženklų po 10, 4 ir 1 ct reikia užklijuoti ant voko paprastos banderolės (laiško), kurios svoris yra pradinis duomuo.
